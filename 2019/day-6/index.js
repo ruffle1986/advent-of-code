ea2012ff8df3fd233785bf9791dbde13a9611449
ruffle1986 <2196208+ruffle1986@users.noreply.github.com>
2021-11-25 13:26:17 +0100

    day 6/part two (using breadth first search)

--- 2019/day-6/index.js
+++ 2019/day-6/index.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 const input = fs.readFileSync('./input.txt').toString().trim().split('\n');
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.adjacent = [];
+  }
+}
+
 function toHashmap(arr) {
   return arr.reduce((map, orbit) => {
     const [parent, child] = orbit.split(')').map(s => s.trim());
@@ -23,4 +30,61 @@ function getTotalOrbits(map) {
   return totalCount;
 }
 
+function toGraph(arr) {
+  return arr.reduce((map, orbit) => {
+    const [parent, child] = orbit.split(')').map(s => s.trim());
+    const parentNode = map.has(parent) ? map.get(parent) : new Node(parent);
+    const childNode = map.has(child) ? map.get(child) : new Node(child);
+    parentNode.adjacent.push(childNode);
+    childNode.adjacent.push(parentNode);
+    
+    if (!map.has(parent)) {
+      map.set(parent, parentNode);
+    }
+    if (!map.has(child)) {
+      map.set(child, childNode);
+    }
+
+    return map;
+  }, new Map());
+}
+
+/**
+ * Breadth first search (bfs)
+ */
+function getShortestPath(source, target, graph) {
+  const queue = [];
+  const visited = {};
+  queue.push([source]);
+  while (queue.length > 0) {
+    const path = queue.shift();
+    const nodeId = path[path.length - 1];
+    if (target !== undefined && nodeId === target) {
+      return path;
+    }
+    if (visited[nodeId]) {
+      continue;
+    }
+    const node = graph.get(nodeId);
+    if (!node) {
+      throw new Error('node not found');
+    }
+    visited[nodeId] = true;
+    for (let i = 0; i < node.adjacent.length; i++) {
+      const adjNodeId = node.adjacent[i].value;
+      if (!visited[adjNodeId]) {
+        const newPath = [...path];
+        newPath.push(adjNodeId);
+        queue.push(newPath);
+      }
+    }
+  } 
+}
+
+function getTotalJumps(path) {
+  // path includes YOU and SAN and total jumps = path.length - 1
+  return path.length - 3;
+}
+
 console.log('Advent of Code (Day 6 / Part One): The answer is ' + getTotalOrbits(toHashmap(input)));
+console.log('Advent of Code (Day 6 / Part two): The answer is ' + getTotalJumps(getShortestPath('YOU', 'SAN', toGraph(input))));
