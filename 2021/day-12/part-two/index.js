7f14529f5672047ead0f2218baae52fc23a544d6
ruffle1986 <2196208+ruffle1986@users.noreply.github.com>
2021-12-12 17:49:06 +0100

    aoc 2021: day 12/part two, pass visited and twice through the queue

--- 2021/day-12/part-two/index.js
+++ 2021/day-12/part-two/index.js
@@ -1,62 +1,43 @@
-
 function isSmall(c) {
   return c === c.toLowerCase();
 }
 
-function count(x, path) {
-  let count = 0;
-  for (let i = 0; i < path.length; i++) {
-    if (path[i] === x) {
-      count++;
-    }
-  }
-  return count;
-}
-
-function hasRepEl(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i+1; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
-function canIaddMore(x, path) {
-  if (x === 'end' && count(x, path) === 1) return false;
-  if (x === 'start' && count(x, path) === 1) return false;
-
-  path = path.filter((p) => {
-    if (p === 'start') return false;
-    if (p === 'end') return false;
-    return isSmall(p);
-  });
-
-
-  return !hasRepEl(path);
-}
-
-module.exports = function (input) {
-  
-  const queue = [['start']];
+module.exports = function (input) { 
+  const queue = [[['start'], { start: 1 }, false]];
   const paths = [];
+
   while (queue.length > 0) {
-    const path = queue.shift();
+    const [path, visited, addedTwice] = queue.shift();
     const lastNode = path[path.length - 1];
+    
     if (lastNode === 'end') {
+      
       paths.push(path);
+
     } else {
+      
       const adj = input[lastNode];
+      
       for (let i = 0; i < adj.length; i++) {
         
         if (isSmall(adj[i])) {
-          if (!path.includes(adj[i]) || canIaddMore(adj[i], path)) {
-            queue.push([...path, adj[i]]);
-          } 
+          
+          if (!visited[adj[i]]) {
+            const newVisited = {
+              ...visited,
+              [adj[i]]: true
+            };
+
+            queue.push([[...path, adj[i]], newVisited, addedTwice]);
+          
+          } else {
+            
+            if (!addedTwice && !['start', 'end'].includes(adj[i])) {
+              queue.push([[...path, adj[i]], visited, true]);
+            }
+          }
         } else {
-          queue.push([...path, adj[i]]);
+          queue.push([[...path, adj[i]], visited, addedTwice]);
         }
       }
     }
